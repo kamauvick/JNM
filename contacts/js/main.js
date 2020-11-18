bb574b51858ce17a50397ab5e8bc40615f752ce4
Victor Waichigo (Techno Brain Global FZE) <v-viwaic@microsoft.com>
2020-11-18 13:51:56 +0300

    Added a scroll down button and changed the banner.

--- contacts/js/main.js
+++ contacts/js/main.js
@@ -52,6 +52,25 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
+    /** Scroll up Button */
+
+    let myBtn= document.getElementById("myBtn");
+    // When the user scrolls down 20px from the top of the document, show the button
+
+    windows.onscroll = function() {scrollFunction()};
     
+    function scrollFunction() {
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            myBtn.style.display = "block";
+        }else{
+            myBtn.style.display = "none";
+        }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    };
 
 })(jQuery);
